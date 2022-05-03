1759a4fc0f4ff4fd7ffc46afab428d9fa3fbe3d4
athrika <athrikapattar@gmail.com>
2022-05-03 22:20:13 +0530

    cm4

---
+++ src/components/InputWithLabel.tsx
@@ -0,0 +1,26 @@
+import "./InputWithLabel.css";
+
+type InputWithLabelProps = {
+  children: React.ReactNode;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  searchText: string;
+  type?: string;
+  id: string;
+};
+
+const InputWithLabel = ({
+  children,
+  onChange,
+  searchText,
+  type = "text",
+  id,
+}: InputWithLabelProps) => {
+  return (
+    <div>
+      <label htmlFor={id}>{children} </label>
+      <input value={searchText} id={id} type={type} onChange={onChange} />
+    </div>
+  );
+};
+
+export default InputWithLabel;
